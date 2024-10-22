96cffaf87136e7401a6f4c611c84f6226bd8da69
Gabe <gabriellemenw@gmail.com>
2024-10-22 17:56:16 -0300

    feat: Adicionar Missoes
    Este commit organiza o sistema de cadastro com novas pastas, Ninja e
    Missoes, além de adicionar o `feat: Adicionar Missoes
    
    Avançar com o projeto seguindo as aulas de Spring Boot.
    
    Este commit organiza o sistema de cadastro com novas pastas, Ninja e Missoes, além de adicionar o `MissoesModel.java`.

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -0,0 +1,33 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_missoes")
+public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String dificuldade;
+
+    private NinjaModel ninja;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(String dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+}
